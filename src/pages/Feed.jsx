7cd7731160fcd6af8b1ede26808636fa16730506
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-27 18:14:07 +0500

    showing sidebar on feed

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
-
-const Feed = () => {
+import {useState,useEffect} from 'react'
+import {Box,Stack,Typography} from '@mui/material'
+import Sidebar from '../components/Sidebar'
+ const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{flexDirection:{sx:'columnn',md:'row'}}}>
+      <Box sx={{height:{sx:'auto',md:'92vh'},borderRight:'1px solid #3d3d3d',px:{sx:0,md:2}}}>
+        <Sidebar />
+        <Typography className='copyright'
+        variant='body2'
+        sx={{mt:1.5,color:'#fff'}}
+        >
+          Build By <a style={{color:"#fff",fontStyle:'italic'}} target='_blank' href='https://github.com/arslanjajja1'>@Arslan Jajja</a>
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 

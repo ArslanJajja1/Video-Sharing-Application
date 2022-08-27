aa436b544b765ae3129bf25ff0fcf7d62e8ed4b7
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-27 21:01:42 +0500

    video component error fix

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,21 +1,23 @@
-import {Stack,Box} from '@mui/material'
-import ChannelCard from './ChannelCard'
-import VideoCard from './VideoCard'
+import { Stack, Box } from "@mui/material";
+import ChannelCard from "./ChannelCard";
+import VideoCard from "./VideoCard";
 
-const Videos = ({videos}) => {
-console.log('Videos',videos)
-  return (
-    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
-        {
-            videos.map((item,index)=>{
-                return <Box key={index}>
-                    {item.id.videoId&&<VideoCard video={item}/>}
-                    {item.id.videoId&&<ChannelCard channelDetail={item}/>}
-                </Box>
-            })
-        }
-    </Stack>
-  )
-}
+const Videos = ({ videos }) => {
+    console.log("Videos", videos);
+    return (
+        <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+            {videos.map((item, index) => {
+                return (
+                    <Box key={index}>
+                        {item.id.videoId && <VideoCard video={item} />}
+                        {item.id.channelId && (
+                            <ChannelCard channelDetail={item} />
+                        )}
+                    </Box>
+                );
+            })}
+        </Stack>
+    );
+};
 
-export default Videos
+export default Videos;

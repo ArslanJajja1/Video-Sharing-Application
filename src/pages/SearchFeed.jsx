466b0f584a722960d04a196f220fc812e85b3570
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-28 09:29:17 +0500

    videos search feature done

--- src/pages/SearchFeed.jsx
+++ src/pages/SearchFeed.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Box, Typography, Stack } from "@mui/material";
+import Videos from "../components/Videos";
+import { fetchFromApi } from "../utils/fetchFromApi";
+import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
-  return (
-    <div>SearchFeed</div>
-  )
-}
+    const [videos, setVideos] = useState([]);
+    const { searchTerm } = useParams();
+    useEffect(() => {
+        fetchFromApi(`search?part=snippet&q=${searchTerm}`).then((data) =>
+            setVideos(data.items)
+        );
+    }, [searchTerm]);
 
-export default SearchFeed
+    return (
+        <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+            <Typography
+                variant="h4"
+                fontWeight="bold"
+                mb={2}
+                sx={{ color: "white" }}
+            >
+                Search results for :{" "}
+                <span style={{ color: "#F31503" }}>{searchTerm}</span>
+            </Typography>
+            <Videos videos={videos} cardWidth="300px" />
+        </Box>
+    );
+};
+
+export default SearchFeed;
